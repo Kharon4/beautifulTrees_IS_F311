5fd1e5cfb06e51d5948cca1f85c3fb0882ce8411
Abhijeet Viswa <abhijeetviswa@gmail.com>
2020-02-22 00:46:50 +0530

    Re-define convertXToNDL and convertYToNDL as macros
    An extra function call is not worth it, espescially if the
    pre-processor can inline calculation

--- beautifulTrees/utils.cpp
+++ beautifulTrees/utils.cpp
@@ -2,20 +2,13 @@
 #include "utils.h"
 #include <GLFW/glfw3.h>
 
+#define convertXToNDL(x) ((2.0 * x / VIEWPORT_WIDTH) - 1)
+#define convertYToNDL(x) ((2.0 * x / VIEWPORT_HEIGHT) - 1)
+
 void drawPoint(int x, int y)
 {
   float destX = convertXToNDL(x);
   float destY = convertYToNDL(y);
 
   glVertex2f(destX, destY);
-}
-
-float convertXToNDL(int x)
-{
-  return 2.0 * x / VIEWPORT_WIDTH - 1;
-}
-
-float convertYToNDL(int y)
-{
-  return 2.0 * y / VIEWPORT_HEIGHT - 1;
 }
